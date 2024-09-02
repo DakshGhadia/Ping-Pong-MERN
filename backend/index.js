f9eebbbff775e17f8001cd08b970854de7864d2f
Daksh <ghadiadaksh@gmail.com>
2024-09-02 08:23:36 -0400

    third commit

--- backend/index.js
+++ backend/index.js
@@ -1,21 +1,23 @@
 import express from "express";
-import { PORT, mongoDBURL } from "./config.js";
+import dotenv from "dotenv";
 import mongoose from "mongoose";
 import router from "./routes/resultRoute.js";
 import cors from "cors";
 
+dotenv.config();
 const app = express();
+const port = process.env.PORT || 4000;
 app.use(express.json());
 app.use(cors());
 app.use("/results", router);
 
 mongoose
-  .connect(mongoDBURL)
+  .connect(process.env.MONGODBURL)
   .then(() => {
     console.log("App connected to database");
 
-    app.listen(PORT, () => {
-      console.log(`App is listening to port: ${PORT}`);
+    app.listen(port, () => {
+      console.log(`App is listening to port: ${port}`);
     });
   })
   .catch((error) => {

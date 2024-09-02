a38c7768a4923c8207cdcd3148750ecf3a4fee1e
Daksh Ghadia <145304879+DakshGhadia@users.noreply.github.com>
2024-09-02 09:56:53 -0400

    used cors

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,9 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 4000;
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: 'https://ping-pong-frontend.onrender.com', // Allow requests from this origin
+}));
 app.use("/results", router);
 
 mongoose
